a0a125871a5ab5f986b2f2e3c22ec2ac6b9cee8e
Damirbek <busurmankulov577@gmail.com>
2022-01-05 15:25:16 +0600

    Repitition Js

---
+++ RepetitionJS/app.js
@@ -0,0 +1,104 @@
+// 1 Переменные
+const lastName = "Damirbek"; //String CamelCase
+// const age = 26; // NUmber 
+// const isProgrammer = true; // Boolean
+
+
+// const _privete = 'preeventdefaul'
+// const $ = 'some value'
+
+// const ife = 'if' зарезервированное слово нельзя писать по переменные 
+const damir54 = '5';
+// const 34asd = 'asd'  error 
+
+// 2 Мутирование
+
+// console.log('Name of human: ' + lastName + ' , but Age :' + age)
+// alert('Name of human: ' + lastName + ' , but Age :' + age)
+
+// console.log(age.toString())
+
+// const firstName2 = prompt('Enter your Lastname')
+// alert(lastName + ' ' + firstName2);
+
+// 3 Operators
+
+// let currentYear = 2022 ;
+// const birthYear = 1999;
+
+// // const age = currentYear - birthYear;
+// const a = 10;
+// const b = 5 ;
+
+// console.log(a - b);
+// console.log(a + b)
+// console.log(a / b)
+// console.log(a * b)
+
+// let c = 32  
+// // c = c + a
+// c += a
+// c -= a
+// c /= a
+
+// c *= a
+
+
+// console.log(currentYear)
+// console.log(--currentYear)
+
+// console.log(c)
+
+// 4 типы данных
+// const isProgrammer = true
+// const fname = 'Damirbek'
+// const age = 22 ;
+// let x ;
+
+// console.log(typeof isProgrammer)
+// console.log(typeof fname)
+// console.log(typeof age)
+// console.log(typeof x)
+// console.log(null)
+
+// 5 Приоритет операторов //Operator's Priority 
+
+// const fullAge = 23;
+// const birthYear = 1999;
+// const currentYear = 2022;
+
+// const isFullage = currentYear - birthYear >= fullAge // 23 >= 23
+
+// console.log(isFullage);
+
+// 6 Условные операторы 
+
+// const courseStatus = 'fail' // ready , fail , pending
+
+// if (courseStatus === 'ready') {
+//     console.log('Course is already  ready and its can learning');
+// }else if(courseStatus === 'pending'){
+//     console.log('Course is  still  in pending');
+// } else {
+//     console.log('The course did not work out');
+// }
+
+// const num1 = 42 ;
+// const num2 = '42';
+
+// console.log(num1 == num2);
+
+const isReady = true;
+
+// if(isReady){
+//     console.log('Done');
+// } else {
+//     console.log('All is not done');
+// }
+// Тернарное выражение 
+isReady ? console.log('Done') : console.log('All is not done');
+
+// 7 Булевая логика 
+
+
+
